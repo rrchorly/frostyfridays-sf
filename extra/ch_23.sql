134b7c241bfe03210b7f3675b821277330054218
David Sanchez <dsmdavid@gmail.com>
2022-11-18 16:56:32 +0000

    challenge 23 (#39)

--- extra/ch_23.sql
+++ extra/ch_23.sql
@@ -1,3 +1,13 @@
+/*
+# run the below line once the variables have been set
+# export db_name=YOUR_DB_NAME
+# export schema_name=YOUR_SCHEMA_NAME
+# export warehouse_name=YOUR_WAREHOUSE_NAME
+
+# line to run:
+# snowsql -c il -f ./ch_23.sql -D stage_name=dvd_frosty_fridays_23 -D db_name=$sf_frosty_db_name -D schema_name=$sf_frosty_schema -D warehouse_name=$sf_frosty_wh_name
+*/
+
 !set variable_substitution=true;
 !define STAGE_NAME = &stage_name;
 !define DB_NAME = &db_name;
