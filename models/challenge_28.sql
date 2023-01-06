e490d613fffa7b6bbe328ef646870ee81c62197c
David Sanchez <dsmdavid@gmail.com>
2023-01-06 17:26:19 +0000

    feat: add challenge 28 (#45)

---
+++ models/challenge_28.sql
@@ -0,0 +1,38 @@
+{{
+  config(
+    materialized = 'view',
+ )
+}}
+--noqa: disable=L057
+WITH covid AS (
+    SELECT * FROM {{ source('marketplace_covid19_starschema', 'ECDC_GLOBAL') }}
+    WHERE last_reported_flag
+),
+
+daily_weather AS (
+    SELECT * FROM {{ source('marketplace_daily_wheather', 'NOAACD2019R') }}
+),
+
+daily_temps AS (
+    SELECT * FROM daily_weather
+    WHERE "Indicator Name" = 'Mean temperature (Fahrenheit)'
+          AND "Measure Name" = 'Value'
+),
+
+final AS (
+
+    SELECT
+        c.country_region AS country,
+        c.date,
+
+        d."Indicator Name",
+        d."Value"
+    FROM covid AS c
+        LEFT JOIN daily_temps AS d
+            ON --c.iso3166_1 = d."Country"
+                c.country_region = d."Country Name"
+    QUALIFY row_number() OVER (PARTITION BY c.country_region ORDER BY d."Stations Name" DESC) = 1
+)
+
+SELECT * FROM final
+ORDER BY country ASC
