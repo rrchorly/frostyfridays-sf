5c67a100f26fc833002cf6aad1d05b547268d467
David Sanchez <dsmdavid@gmail.com>
2023-04-04 23:21:45 +0100

    Ch38 (#55)
    * add challenge_38
    
    really not well suited for dbt.
    Had to be creative with the usage of variables
    docs: update doc block

--- models/challenge_38_01_employees.sql
+++ models/challenge_38_01_employees.sql
@@ -21,9 +21,7 @@ SELECT * FROM base
 WHERE
 
     {% if var('ch38_init', false) %}
-  {{ log('init_38_01', info=True) }}
   1=2
 {% else %}
-  {{- log('regular 38_01', info=True) -}}
-  true
+        true
     {% endif %}
