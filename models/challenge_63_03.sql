813e1f2feaa903105d6c8947a47010c80b42e296
dsmdavid <root@localhost>
2023-09-15 10:41:45 +0000

    docs: add some explanations

--- models/challenge_63_03.sql
+++ models/challenge_63_03.sql
@@ -12,10 +12,12 @@
         WHERE query_text LIKE 'create or replace transient table {{ ref("challenge_63_02") }}%' 
         AND start_time > dateadd('day',-1,current_date())
         ORDER BY start_time DESC;
-{% endset %}
+    {% endset %}
+    {# -- get and print query_id #}
     {% set results = run_query(get_query_id_statement) %}
     {% set last_query_id = results.columns[0].values()[0] %}
     {{ log('last_query_id is:\t' ~ last_query_id, info=True) }}
+    {# -- query to obtain the row multiplier #}
     WITH
         stats AS (
             SELECT *
@@ -37,6 +39,7 @@
     LIMIT 1
 
 {% else %}
+    {# -- dummy query when not running this model #}
     SELECT
         'model not enabled, needs to be run with var(ch63) or var(run_all)'
         AS model_status
